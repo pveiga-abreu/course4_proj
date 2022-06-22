43a40ef9341797babb0e5713c4516711a8ead73b
Paulo Abreu <pveiga.abreu@gmail.com>
2022-06-22 01:42:01 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -38,6 +38,9 @@ class Dev(Configuration):
     CSRF_COOKIE_SAMESITE = "None"
     SESSION_COOKIE_SAMESITE = "None"
 
+    CELERY_RESULT_BACKEND = "django-db"
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
+
     # Application definition
 
     INSTALLED_APPS = [
@@ -49,6 +52,7 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'movies',
 				'gh',
+				'django_celery_results',
     ]
 
     MIDDLEWARE = [
--- course4_proj/urls.py
+++ course4_proj/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import movies.views
 import gh.views
 
 
 urlpatterns = [
     path("", gh.views.index),
     path('admin/', admin.site.urls),
+    path("search/", movies.views.search, name="search"),
+    path(
+        "search-wait/<uuid:result_uuid>/", movies.views.search_wait, name="search_wait"
+    ),
+    path("search-results/", movies.views.search_results, name="search_results"),
 ]
